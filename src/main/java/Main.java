8f7130cea5dd7897eb4e9bceac1cb7c4db588bd3
Lev Erminson <erminson@gmail.com>
2021-04-13 19:12:49 +0300

    Create test pipeline in Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,25 @@
-import java.util.Collection;
+import exception.DBException;
+import service.DBService;
+import model.User;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello JDBC");
+
+        final DBService dbService = new DBService(DBService.DBType.SQLITE);
+        try {
+            dbService.createTable();
+            dbService.saveUser("Lev");
+            dbService.saveUser("Ilnar");
+            dbService.saveUser("Radik");
+
+            List<User> users = dbService.getAllUsers();
+            System.out.println(users);
+            dbService.cleanUp();
+        } catch (DBException e) {
+            e.printStackTrace();
+        }
     }
 }
